ef273a22a71597360d0d7872a134c754df3c2b7d
Robin Ambert <55240427+robambert@users.noreply.github.com>
2024-04-22 13:50:23 +0200

    add dressing, telescope, which-key

--- lua/robin/plugins/telescope.lua
+++ lua/robin/plugins/telescope.lua
@@ -6,10 +6,10 @@ return {
 
 		local keymap = vim.keymap
 
-		keymap.set('n', '<leader>ff', builtin.find_files, {})
-		keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		keymap.set('n', '<leader>fb', builtin.buffers, {})
-		keymap.set('n', '<leader>fh', builtin.help_tags, {})
+		keymap.set('n', '<leader>ff', builtin.find_files, {desc = '[F]ind [F]iles'})
+		keymap.set('n', '<leader>fg', builtin.live_grep, {desc = '[F]ind [G]rep'})
+		keymap.set('n', '<leader>fb', builtin.buffers, {desc = '[F]ind [B]uffers'})
+		keymap.set('n', '<leader>fh', builtin.help_tags, {desc = '[F]ind [H]elpTags'})
 
 	end
 }
